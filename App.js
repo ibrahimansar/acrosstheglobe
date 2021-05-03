08c1d2b0354f2f6f8d90ff1c61f88f6e1cf53eb1
Mohamed Ibrahim Ansari <54184745+ibrahimansar@users.noreply.github.com>
2021-05-04 02:53:00 +0530

    .

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Navigator from "./app/routes/drawer";
-// import Search from "./app/screens/Search";
 import { LogBox } from 'react-native';
 
 LogBox.ignoreAllLogs();
 
 export default function App() {
   return (
-    // <Search />
       <Navigator />
   );
 }
--- app/screens/ImagesScreen.js
+++ app/screens/ImagesScreen.js
@@ -1,44 +1,77 @@
 import React, { useEffect, useState } from 'react';
-import { FlatList, StyleSheet, Text, View, Button, TextInput, SafeAreaView } from 'react-native';
+import { FlatList, StyleSheet, Text, ScrollView,  View, Button, TextInput, SafeAreaView, ActivityIndicator } from 'react-native';
 import Card from "../component/Card";
 import Constants from "expo-constants";
 import useApi from "../hooks/useApi";
 import { MaterialIcons } from '@expo/vector-icons';
+import { Icon } from 'react-native-elements';
+import { Footer } from 'native-base';
 
 export default ImagesScreen = ({ navigation }) => {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState('cat');
+  const [page, setPage] = useState(1);
 
   const openMenu = () => {
     navigation.openDrawer();
   }
+
+  const perPage = page * 6;
   
-  const val = "cat";
-  const getApis = useApi("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=6f102c62f41998d151e5a1b48713cf13&format=json&nojsoncallback=1&extras=url_s&text=" + val)
+  const val = search;
+  const url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=6f102c62f41998d151e5a1b48713cf13&format=json&nojsoncallback=1&extras=url_s&text=" + val + "?page= " + page + "&per_page=" + perPage;
+  const getApis = useApi(url)
 
   useEffect(() => {
     getApis.request();
   })
 
-  useEffect(() => {
-    function clickHandler(){
-    fetch("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=6f102c62f41998d151e5a1b48713cf13&format=json&nojsoncallback=1&extras=url_s&text=" + val)
-      .then((response) => response.json())
-      .then((json) => setData(json.photos))
-      .catch((error) => console.error(error))
-      .finally(() => setLoading(false));
-    };
+  
+  function clickHandler(){
+     setLoading(true)
+     fetch(url)
+    .then((response) => response.json())
+    .then((json) => setData(json.photos))
+    .catch((error) => console.error(error))
+    .finally(() => setLoading(false));
+  };
 
+  useEffect(() => {
     clickHandler();
   }, []);
+
+  function clickHandlerMore(){
+     setPage(page + 1)
+    //  fetch(url)
+    // .then((response) => response.json())
+    // .then((json) => setData(json.photos))
+    // .catch((error) => console.error(error))
+    // .finally(() => setLoading(false));
+  };
+
+  useEffect(() => {
+    clickHandlerMore();
+  }, []);
+
+  // const loadMore = () => {
+  //   setPage(page + 1);
+  // } 
+  
+  const renderFooter = () => {
+    return(
+      <View style={styles.footer}>
+        <ActivityIndicator color="black" size="large"/>
+      </View>
+    )
+  }
 
   return (
     <SafeAreaView style={styles.screen}>
         {getApis.error && (
           <>
             <AppText>Couldn't retrieve the listings.</AppText>
-            <Button title="Retry" onPress={getApis.request} />
+            <Button title="Retry" onPress={() => {clickHandler()}} />
           </>
         )}
 
@@ -49,13 +82,21 @@ export default ImagesScreen = ({ navigation }) => {
             <MaterialIcons name='menu' size={28} onPress={openMenu} style={styles.icon} />
             <View style={styles.TextInput}>
               <TextInput
-              placeholder="Search.." />
+                placeholder="Search.."
+                onChangeText={(text) => setSearch(text)}
+               />
             </View>
+            <Icon name = "search" onPress= {() => {clickHandler()}}/>  
           </View>
-          <View style={{}}>
+
+          
+          <ScrollView style={styles.list} onMomentumScrollEnd={() => {clickHandler()}} >
             <FlatList
               data={data.photo}
               numColumns= '2'
+              ListFooterComponent={renderFooter}
+              onEndReached={() => {console.log("reached")}}
+              onEndReachedThreshold ={0.3}
               keyExtractor={({ id }, index) => id}
               renderItem={({ item }) => (
                 <Card
@@ -63,7 +104,7 @@ export default ImagesScreen = ({ navigation }) => {
                 />
               )}
             />
-          </View>
+          </ScrollView>
         </>  
       )}        
     </SafeAreaView>
@@ -80,27 +121,33 @@ const styles = StyleSheet.create({
   view: {
     marginBottom: 20
   },
-    header: {  
+    header: {
+    display: 'flex',  
     width: '100%',
     height: 50,
     flexDirection: 'row',
-    alignItems: "baseline",
+    alignItems: "center",
     justifyContent: 'center',
   },
-  headerText: {
-    fontWeight: 'bold',
-    fontSize: 30,
-    color: '#333',
-    letterSpacing: 1,
-  },
   icon: {
-    position: 'absolute',
-    left: 16,
+    left: 6,
   },  
   TextInput: {
-    width: "100%",
-    height: "100%",
+    width: "70%",
+    height: "70%",
     paddingLeft: 8,
+    paddingRight: 8,
+    marginLeft: 20,
     fontSize: 16,
+    borderRadius: 15,
+    backgroundColor: "#f7f3f3",
+    borderWidth: 0.5
+  },
+  footer: {
+    marginTop: 10,
+    alignItems: "center"
+  },
+  list: {
+    paddingBottom: 65
   }
 });
